35d05b2add604db60caf7a0ca5486099b09c5270
andrew <sosljuk8@mail.ru>
2016-12-16 02:29:39 +0300

    table exemple

--- views/docs_list_view.php
+++ views/docs_list_view.php
@@ -1,16 +1,17 @@
-kонтент списка доков
-<table border="1" bordercolor="black" class="table table-striped">
-        <thead>
-            <tr>
-                <th width="150">колонок</th>
-                <th width="150">строк</th>
-            </tr>
-        </thead>
-        <? foreach($data as $item):?>
-        <tr>
-            <td><?= $item['col']; ?></td>
-            <td><?= $item['row']; ?></td>
-        </tr>
-        <? endforeach;?>
-
-    </table>
+<div class="tbl">
+    <div class="row hdr" style="min-height: 20px;">
+        <?php for ($i = 0; $i < 20; $i++) { ?>
+        <div class="cel cel<?= $i?>" data-id="{0,<?= $i?>}" style="float: left; width: 60px;border: 1px solid black;min-height: 20px;">0,<?= $i?></div>
+        <?php }?>
+        <div style="clear: both"></div>
+    </div>
+    <?php for ($i = 1; $i < 50; $i++) { ?>
+    <div class="row str" style="min-height: 20px;">
+        <?php for ($j = 0; $j < 20; $j++) { ?>
+        <div class="cel cel<?= $i?>" data-id="{<?= $i?>,<?= $j?>}" style="float: left; width: 60px;border: 1px solid black;min-height: 20px;"><?= $i?>,<?= $j?></div>
+        <?php }?>
+        <div style="clear: both"></div>
+    </div>
+    <?php }?>
+    
+</div>
